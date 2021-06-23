9f755e04aff097141f85ea8747c379420667d06d
AnkitPradhan26 <44779510+AnkitPradhan26@users.noreply.github.com>
2021-06-23 21:24:21 +0530

    Create autobotreact.py

---
+++ autobotreact.py
@@ -0,0 +1,19 @@
+import os 
+import opencv2
+import casgrand
+import argparse
+import pandas as pd
+
+parser = argparse.ArgumentParser(
+    description="Check upon the environment and react."
+)
+
+if object.dist < 2 #cms
+print(action)
+#open surrounding helmet glass
+reactTOenvironment=(dist)
+object.dist = reactTOenvironment
+else
+continue
+break
+end
